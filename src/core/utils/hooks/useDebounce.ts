8d3cec6443aa99ac3db4377fbef0daef9ccb7c42
osmarpetry <osmarpetry@gmail.com>
2020-11-06 00:05:13 -0300

    now i have the heroes offline :)

---
+++ src/core/utils/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const delayHandller = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(delayHandller);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce
