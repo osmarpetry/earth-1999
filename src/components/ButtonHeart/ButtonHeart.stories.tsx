5218abbf4014de414c77bf3bec142a9da2a6f04d
osmarpetry <osmarpetry@gmail.com>
2020-11-07 01:31:57 -0300

    heart button implemention

---
+++ src/components/ButtonHeart/ButtonHeart.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import ButtonHeart, { ButtonHeartProps } from 'components/ButtonHeart';
+
+export default {
+  title: 'Components/ButtonHeart',
+  component: ButtonHeart,
+} as Meta;
+
+const Template: Story<ButtonHeartProps> = (args) => <ButtonHeart {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  value: true
+};
---
+++ src/components/ButtonHeart/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeartIcon = ({ fill }: { fill: string }) => (
+  <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1">
+    <title>Path Copy 2@1,5x</title>
+    <g
+      id="Page-1"
+      stroke="none"
+      stroke-width="1"
+      fill="none"
+      fill-rule="evenodd"
+    >
+      <g
+        id="Guide"
+        className="teste"
+        transform="translate(-231.000000, -828.000000)"
+        fill-rule="nonzero"
+        stroke="#FF0000"
+        fill={fill}
+        stroke-width="2"
+      >
+        <path
+          d="M248.428712,839.053345 L241.268331,846.885348 C241.199896,846.960594 241.102641,847 241.001802,847 C240.900941,847 240.803707,846.960594 240.735272,846.885348 L233.574892,839.053345 C231.475036,836.760342 231.475036,833.030654 233.574892,830.737651 C234.59779,829.616232 235.962887,829 237.418006,829 C238.750677,829 240.011307,829.515925 241.001802,830.465354 C241.992297,829.515925 243.249322,829 244.581993,829 C246.037112,829 247.405792,829.616232 248.432318,830.737651 C250.524963,833.030654 250.521357,836.760342 248.428712,839.053345 Z"
+          id="Path-Copy-2"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
+
+const ButtonStyled = styled.button`
+  background: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 4s ease;
+`;
+
+export interface ButtonHeartProps {
+  value: boolean;
+  onChange?: () => void;
+}
+
+export default function ButtonHeart({
+  value,
+  onChange,
+}: ButtonHeartProps) {
+  return (
+    <ButtonStyled onClick={onChange}>
+      <HeartIcon fill={value ? 'red' : 'none'} />
+    </ButtonStyled>
+  );
+}
