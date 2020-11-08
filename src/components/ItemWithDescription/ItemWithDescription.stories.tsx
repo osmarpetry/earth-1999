d5e6ac9144215f61789c91cd1d901678b7d98963
osmarpetry <osmarpetry@gmail.com>
2020-11-07 22:07:01 -0300

    item description with log first step

---
+++ src/components/ItemWithDescription/ItemWithDescription.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import ItemWithDescription, {
+  ItemWithDescriptionProps,
+} from 'components/ItemWithDescription';
+
+import { ReactComponent as HQLogo } from 'assets/icones/book/Group@1,5x.svg';
+import { ReactComponent as HeroLogo } from 'assets/icones/heroi/noun_Superhero_2227044@1,5x.svg';
+
+export default {
+  title: 'Components/ItemWithDescription',
+  component: ItemWithDescription,
+} as Meta;
+
+const Template: Story<ItemWithDescriptionProps> = (args) => (
+  <ItemWithDescription {...args} />
+);
+
+export const OneLine = Template.bind({});
+OneLine.args = {
+  itemName: 'Quadrinhos',
+  description: '3.000',
+  descriptionLogo: <HQLogo />,
+  hasTwoLines: true,
+};
+
+export const OneLineNoLogo = Template.bind({});
+OneLineNoLogo.args = {
+  itemName: 'Último quandrinho:',
+  description: '13 fev. 2020',
+};
+
+
+export const TwoLines = Template.bind({});
+TwoLines.args = {
+  itemName: 'Hero:',
+  descriptionLogo: <HeroLogo />,
+};
---
+++ src/components/ItemWithDescription/index.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+export interface ItemWithDescriptionProps {
+  description?: string;
+  descriptionLogo?: ReactNode;
+  hasTwoLines?: boolean;
+  itemName: string;
+}
+
+const ItemWithDescriptionWrapper = styled.div<{ hasTwoLines: boolean }>`
+  width: fit-content;
+  display: flex;
+  flex-direction: ${({ hasTwoLines }) => (hasTwoLines ? 'column' : 'row')};
+  align-items: ${({ hasTwoLines }) => (hasTwoLines ? 'center' : 'left')}; ;
+`;
+
+const Description = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  span,
+  p {
+    margin: 0;
+  }
+
+  span:last-child {
+    margin-left: 10px;
+  }
+`;
+
+const ItemName = styled.p`
+  font-weight: 600;
+  white-space: nowrap;
+`;
+
+export default function ItemWithDescription({
+  description,
+  descriptionLogo,
+  hasTwoLines = false,
+  itemName,
+}: ItemWithDescriptionProps) {
+  return (
+    <ItemWithDescriptionWrapper hasTwoLines={hasTwoLines}>
+      <ItemName>{itemName}</ItemName>
+      <Description>
+        {descriptionLogo && <span>{descriptionLogo}</span>}
+        {description && (
+          <span>
+            <p>{description}</p>
+          </span>
+        )}
+      </Description>
+    </ItemWithDescriptionWrapper>
+  );
+}
