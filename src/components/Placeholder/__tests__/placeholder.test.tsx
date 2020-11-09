f88b805d58fccd3b64cb90a397e93333816c5943
osmarpetry <osmarpetry@gmail.com>
2020-11-09 18:34:33 -0300

    placeholder test

--- src/components/Placeholder/__tests__/placeholder.test.tsx
+++ src/components/Placeholder/__tests__/placeholder.test.tsx
@@ -7,75 +7,75 @@ afterEach(cleanup)
 
 it('should render child when sucess and is not empty', () => {
   const status = 'success'
-  const { queryByText, queryByTestId } = render(
-    <Placeholder status={status} isEmpty={false} contentsName='dados'>
+  const { queryByText } = render(
+    <Placeholder status={status} isEmpty={false} contentsName='heroes'>
       <div>
         <p>Content is loadead</p>
       </div>
     </Placeholder>
   )
 
-  expect(queryByTestId('empty')).toBeFalsy()
+  expect(queryByText('No heroes found!')).toBeFalsy()
   expect(queryByText('Content is loadead')).toBeTruthy()
-  expect(queryByTestId('error')).toBeFalsy()
+  expect(queryByText('Ops! Something wrong just happen with the heroes data!')).toBeFalsy()
 })
 
 it('should render specific message when sucess but is empty', () => {
   const status = 'success'
-  const { queryByText, queryByTestId } = render(
-    <Placeholder status={status} isEmpty={true} contentsName='dados'>
+  const { queryByText } = render(
+    <Placeholder status={status} isEmpty={true} contentsName='heroes'>
       <div>
         <p>Content is loadead</p>
       </div>
     </Placeholder>
   )
 
-  expect(queryByTestId('empty')).toBeTruthy()
+  expect(queryByText('No heroes found!')).toBeTruthy()
   expect(queryByText('Content is loadead')).toBeFalsy()
-  expect(queryByTestId('error')).toBeFalsy()
+  expect(queryByText('Ops! Something wrong just happen with the heroes data!')).toBeFalsy()
 })
 
 it('should render error message when status error and is not empty', () => {
   const status = 'error'
-  const { queryByText, queryByTestId } = render(
-    <Placeholder status={status} isEmpty={false} contentsName='dados'>
+  const { queryByText } = render(
+    <Placeholder status={status} isEmpty={false} contentsName='heroes'>
       <div>
         <p>Content is loadead</p>
       </div>
     </Placeholder>
   )
 
-  expect(queryByTestId('empty')).toBeFalsy()
+  expect(queryByText('No heroes found!')).toBeFalsy()
   expect(queryByText('Content is loadead')).toBeFalsy()
-  expect(queryByTestId('error')).toBeTruthy()
+  expect(queryByText('Ops! Something wrong just happen with the heroes data!')).toBeTruthy()
 })
 
 it('should render error message when status error and is empty', () => {
   const status = 'error'
-  const { queryByText, queryByTestId } = render(
-    <Placeholder status={status} isEmpty={true} contentsName='dados'>
+  const { queryByText } = render(
+    <Placeholder status={status} isEmpty={true} contentsName='heroes'>
       <div>
         <p>Content is loadead</p>
       </div>
     </Placeholder>
   )
 
-  expect(queryByTestId('empty')).toBeFalsy()
+  expect(queryByText('No heroes found!')).toBeFalsy()
   expect(queryByText('Content is loadead')).toBeFalsy()
-  expect(queryByTestId('error')).toBeTruthy()
+  expect(queryByText('Ops! Something wrong just happen with the heroes data!')).toBeTruthy()
 })
 
 it('should render the content when status is loading and is empty', () => {
   const status = 'loading'
-  const { queryByText, queryByTestId } = render(
-    <Placeholder status={status} isEmpty={true} contentsName='dados'>
+  const { queryByText } = render(
+    <Placeholder status={status} isEmpty={true} contentsName='heroes'>
       <div>
         <p>Content with skeleton is loading</p>
       </div>
     </Placeholder>
   )
 
-  expect(queryByTestId('empty')).toBeFalsy()
+  expect(queryByText('No heroes found!')).toBeFalsy()
   expect(queryByText('Content with skeleton is loading')).toBeTruthy()
-  expect(queryByTestId('error')).toBeFalsy()
+  expect(queryByText('Ops! Something wrong just happen with the heroes data!')).toBeFalsy()
 })
